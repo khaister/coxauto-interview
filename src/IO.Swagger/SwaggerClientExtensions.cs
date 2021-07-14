3cd18cbef59fc5a2e844537122468c0de84ce08e
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2021-07-13 19:02:23 -0700

    Add generated client code

---
+++ src/IO.Swagger/SwaggerClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using IO.Swagger.Api;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class SwaggerClientExtensions
+    {
+        public static IServiceCollection AddSwaggerClientServices(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<IDataSetApi, DataSetApi>()
+                .AddScoped<IDealersApi, DealersApi>()
+                .AddScoped<IVehiclesApi, VehiclesApi>()
+                .AddScoped<IClientApi, ClientApi>();
+        }
+    }
+}
